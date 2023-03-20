450128c496ce0852d5d6af1e41cc379a7a8879bf
Anurag Kumar Singh <singh.ask@outlook.com>
2023-03-20 16:36:46 +0000

    added firebase SDK

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import Head from "next/head";
-import Image from "next/image";
-import buildspaceLogo from "../assets/buildspace-logo.png";
 import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+
+
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: prpcess.env,measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const Home = () => {
   const [userInput, setUserInput] = useState("");
   const [apiOutput, setApiOutput] = useState("");
